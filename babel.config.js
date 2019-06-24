247afab8eb410607425fbdac48c702051780269d
eduardo <eduloni3@gmail.com>
2019-06-24 22:48:51 +0100

    cleaner imports

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
+/** eslint-disable */
 module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        require.resolve('babel-plugin-module-resolver'),
+        {
+          root: ['./'],
+          alias: {
+            src: './src',
+            actions: './src/actions',
+            components: './src/components',
+            constants: './src/constants',
+            containers: './src/containers',
+            reducers: './src/reducers',
+            utils: './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
