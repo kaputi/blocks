ace74a25d19a391da1f29303e649de667c437152
eduardo <eduloni3@gmail.com>
2019-06-30 09:55:26 +0100

    fixed positions

--- src/utils/pieceFunctions.js
+++ src/utils/pieceFunctions.js
@@ -5,7 +5,7 @@ import random from 'utils/random'
 export const mapCoords = (pieceMap, x, y) => {
   const coordMap = pieceMap
     .map((pair) => [pair[0] + x, pair[1] + y])
-    .filter((coordPair) => coordPair[1] > 0) // removes blocks above the game area
+    .filter((coordPair) => coordPair[1] >= 0) // removes blocks above the game area
   return coordMap
 }
 
@@ -20,9 +20,9 @@ export const mapPixels = (coordMap) => {
 export const calculatePiece = ({
   id = random(0, pieceData.length - 1),
   position = 0,
-  x = 5,
-  y = 1,
-}) => {
+  x = 4,
+  y = -1,
+}={}) => {
   const { pieceMap, color } = pieceData[id]
 
   const coordMap = mapCoords(pieceMap[position], x, y)
