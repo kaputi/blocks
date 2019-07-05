61078005984157ae4eecb6a489404a172e978c96
eduardo <eduloni3@gmail.com>
2019-07-05 08:11:01 +0100

    app.js

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Provider } from 'react-redux'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import GameArea from 'containers/GameArea'
+import Main from 'containers/Main'
+
+import store from 'src/store'
 
 const styles = StyleSheet.create({
   container: {
@@ -16,4 +14,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
+
+const App = () => {
+  // store.subscribe(() => console.log(store.getState()))
+  console.log('==================================')
+  return (
+    <Provider store={store}>
+      <View style={styles.container}>
+        {/* <GameArea /> */}
+        <Main />
+      </View>
+    </Provider>
+  )
+}
+export default App
